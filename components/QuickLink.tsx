b1f4737a5842d5b8290e196177366c9973769201
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2022-09-13 19:06:22 -0700

    open in new tab

--- components/QuickLink.tsx
+++ components/QuickLink.tsx
@@ -1,6 +1,6 @@
 export default function QuickLink(props:{to: string, title: string, className?: string}){
     return (
-        <a href={props.to} className={`block px-6 py-4 text-center text-lg text-white rounded-md font-bold ${props.className}`}>
+        <a href={props.to} target="_blank" className={`block px-6 py-4 text-center text-lg text-white rounded-md font-bold ${props.className}`}>
             {props.title}
         </a>
     )
