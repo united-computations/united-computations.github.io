9c8c23c99443ea812ae8a1e098748a90349175a4
Julia Kang <julia7hk@gmail.com>
2023-09-11 14:21:51 -0700

    update signup link and add nav to links page

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,21 +1,29 @@
 import Head from 'next/head';
+import Nav from '../components/Nav';
 import QuickLink from '../components/QuickLink';
 
 export default function Links() {
     return (
-        <div className="flex flex-col pt-20">
+        <div>
             <Head>
                 <title>United Computations • Quick Links</title>
                 <meta name="description" content="One of the longest-running CS clubs at Gunn!" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1 className='block text-center text-4xl font-extrabold mb-12'>Gunn United Computations</h1>
-            <div className="flex flex-col mx-auto gap-4">
-                <QuickLink className='bg-rose-500' to='https://forms.gle/oPyFGgxfSsEEc2wK9' title='Mailing List Signup' />
-                <QuickLink className='bg-red-500' to='https://docs.google.com/forms/d/e/1FAIpQLSfHF5w1sZc5pjj1PBjm9udRRgl60IeWxEf4Y0lAXCETPfuN4g/viewform?entry.676984385=United%20Computations' title='Attendance' />
-                <QuickLink className='bg-orange-500' to='https://tinyurl.com/gunnucdiscord' title='Discord Server' />
-                <QuickLink className='bg-amber-500' to='/' title='Our Website' />
-                <QuickLink className='bg-yellow-500' to='https://gunnhacks.com' title='GunnHacks' />
+
+            <Nav />
+
+            <div className="flex flex-col pt-20">
+                <h1 className='block text-center text-4xl font-extrabold mb-12'>Gunn United Computations</h1>
+                <div className="flex flex-col mx-auto gap-4">
+                    <QuickLink className='bg-rose-500' to='https://docs.google.com/forms/d/e/1FAIpQLSd-lEAiVKMF5PeXsWF2LBUxknTchptL0GeJ3rJwS_wfkGTKVw/viewform' title='Mailing List Signup' />
+                    <QuickLink className='bg-red-500' to='https://docs.google.com/forms/d/e/1FAIpQLSfHF5w1sZc5pjj1PBjm9udRRgl60IeWxEf4Y0lAXCETPfuN4g/viewform?entry.676984385=United%20Computations' title='Attendance' />
+                    <QuickLink className='bg-orange-500' to='https://tinyurl.com/gunnucdiscord' title='Discord Server' />
+                    <QuickLink className='bg-amber-500' to='/' title='Our Website' />
+                    <QuickLink className='bg-yellow-500' to='https://gunnhacks.com' title='GunnHacks' />
+                </div>
             </div>
+
+
         </div>)
 }
