4d376352f8883eb0ae93c3d49020a5b4009a6941
Julia Kang <julia7hk@gmail.com>
2023-09-11 15:00:56 -0700

    update officers

--- components/Officers.tsx
+++ components/Officers.tsx
@@ -1,6 +1,6 @@
-import {ReactNode} from 'react';
+import { ReactNode } from 'react';
 import Section from './Section';
-import {SiReact, SiNextdotjs, SiVuedotjs, SiNuxtdotjs, SiSvelte, SiTypescript, SiPython, SiJava, SiKotlin, SiRust, SiCsharp} from 'react-icons/si';
+import { SiReact, SiNextdotjs, SiVuedotjs, SiNuxtdotjs, SiSvelte, SiTypescript, SiPython, SiJava, SiKotlin, SiRust, SiCsharp } from 'react-icons/si';
 
 
 // The "Meet our officers" section on the home page.
@@ -12,32 +12,29 @@ export default function Officers() {
             <h2 className="font-bold text-4xl mb-6">Meet our officers:</h2>
 
             <div className="max-w-3xl mx-auto divide-y divide-tertiary/50">
-                <OfficerCard name="Saumya Singhal" position="Co-president" src="/officers/saumya.png" languages={['vue', 'nuxt', 'ts']}>
-                    Saumya Singhal is an avid Vue developer, math addict, and founder of YMath and Gunn.One. She is also
-                    the vice president of Physics Club and Math Circle and the founder and director of the JLS Math
-                    Circle.
+                <OfficerCard name="Vivien Chen" position="Co-president" src="/officers/vivien.png" languages={['java', 'py']}>
+                    Vivien Chen is a Python and Scheme enthusiast interested in learning more about AI. She is also a
+                    part of GRT. Sometimes she attempts to competitive program.
                 </OfficerCard>
-                <OfficerCard name="Alec Petridis" position="Co-president" src="/officers/alec.png" languages={['java', 'rs']} reverse>
-                    Alec Petridis is an outspoken Rustacean and Antipodean. He enjoys finding exploits, participating in
-                    CTFs, and doing deep dives into lower-level systems like kernels and the V8 compiler.
+                <OfficerCard name="Julia Kang" position="Co-president" src="/officers/julia.png" languages={['react', 'next', 'ts', 'py', 'java']} reverse>
+                    Julia Kang is an avid programmer who is interested in web development and machine learning. She has made
+                    and maintained several web apps and also has some experience with NLP.
                 </OfficerCard>
-                <OfficerCard name="Kevin Yu" position="Co-vice president" src="/officers/kevin.png" languages={['react', 'next', 'ts', 'cs', 'kt', 'svelte']}>
-                    Kevin Yu is a TypeScript enthusiast and co-founder of WATT. He is also the SEC Tech Commissioner, a
-                    member of the GRT controls subgroup, and the GRT webmaster.
-                </OfficerCard>
-                <OfficerCard name="Roger Fan" position="Co-vice president" src="/officers/roger.JPG" languages={['react', 'next', 'ts']} reverse>
+                <OfficerCard name="Roger Fan" position="Co-vice president" src="/officers/roger.JPG" languages={['react', 'next', 'ts']}>
                     Roger Fan is a competitive mathlete and WATT contributor. He is also the co-vice president of Math
                     Circle and a devout Figma enthusiast.
                 </OfficerCard>
-                <OfficerCard name="Anthony Ho" position="Secretary" src="/officers/anthony.jpg" languages={['java', 'py']}>
-                    Anthony Ho is a junior at Gunn High School. He has experience in Java and Python, and is most interested
+                <OfficerCard name="Anthony Ho" position="Co-vice president" src="/officers/anthony.jpg" languages={['java', 'py']} reverse>
+                    Anthony Ho is a senior at Gunn High School. He has experience in Java and Python, and is most interested
                     in exploring data science applications across different domains. Anthony has been involved in organizing
                     GunnHacks since freshman year and is excited to bring the best possible hacker experience to this year's
                     iteration. In his free time, he enjoys swimming, playing piano, and traveling.
                 </OfficerCard>
-                <OfficerCard name="Vivien Chen" position="Treasurer" src="/officers/vivien.png" languages={['java', 'py']} reverse>
-                    Vivien Chen is a Python and Scheme enthusiast interested in learning more about AI. She is also a
-                    rookie in GRT. Sometimes she attempts to competitive program.
+                <OfficerCard name="Corbin Platti" position="Secretary" src="/officers/corbin.png" languages={[]}>
+                    {/* Corbin Platti is a ___. */}
+                </OfficerCard>
+                <OfficerCard name="Stella Su" position="Treasurer" src="/officers/stella.png" languages={[]} reverse>
+                    {/* Stella Su is a ___. */}
                 </OfficerCard>
             </div>
         </Section>
@@ -49,7 +46,7 @@ type OfficerCardProps = {
     src: string, children: ReactNode, languages?: Language[], reverse?: boolean
 }
 function OfficerCard(props: OfficerCardProps) {
-    const {name, position, src, children, languages, reverse} = props;
+    const { name, position, src, children, languages, reverse } = props;
     return (
         <div className={'flex gap-8 md:gap-10 py-7' + (reverse ? ' flex-row-reverse' : '')}>
             <img src={src} alt={name} className="w-24 h-24 md:w-36 md:h-36 rounded-full object-cover flex-none" />
@@ -70,7 +67,7 @@ function OfficerCard(props: OfficerCardProps) {
 }
 
 type Language = "react" | "next" | "vue" | "nuxt" | "svelte" | "ts" | "py" | "java" | "kt" | "rs" | "cs";
-function LanguageIcon(props: {language: Language}) {
+function LanguageIcon(props: { language: Language }) {
     switch (props.language) {
         case 'react': return <SiReact />;
         case 'next': return <SiNextdotjs />;
